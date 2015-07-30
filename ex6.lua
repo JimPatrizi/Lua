d50810192813bd31b918710312dbf3a48380e26e
James Patrizi <jp15mail66@gmail.com>
2015-07-30 12:11:40 -0400

    ex6 done learned lua tables

---
+++ ex6.lua
@@ -0,0 +1,8 @@
+cats = {10, 11, 12, 8, 100}
+print("The average height between these cats is", (cats[1] + cats[2] + cats[3] + cats[4] + cats[5]) / 5)
+print(#cats)--prints number of elements.
+random = {"hello",12,cats}
+print(#random)
+print(random[1])
+print(random[2])
+print(random[3][5])
